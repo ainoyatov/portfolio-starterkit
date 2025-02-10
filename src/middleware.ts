a3a756c1e6b1a92daf5a5e65aabb3094d38cc2f0
Aki Inoyatov <info@gravityelements.com>
2025-02-10 14:27:43 -0700

    remove old code

--- src/middleware.ts
+++ src/middleware.ts
@@ -37,36 +37,3 @@ export const config = {
     // Example matches: '/', '/en/about', '/fr/contact', '/ja/products'
   ]
 }
-
-
-
-
-
-// import createMiddleware from 'next-intl/middleware'
-// import { NextRequest } from 'next/server'
-// import { locales } from './i18n'
-// import { localePrefix } from './navigation'
-
-// type CustomMiddleware = (req: NextRequest) => Promise<NextRequest>
-
-// const customMiddleware: CustomMiddleware = async req => {
-//   console.log('Custom middleware executed before next-intl')
-//   return req
-// }
-
-// const intlMiddleware = createMiddleware({
-//   locales,
-//   defaultLocale: 'en',
-//   localePrefix
-// })
-
-// export default async function middleware(
-//   req: NextRequest
-// ): Promise<ReturnType<typeof intlMiddleware>> {
-//   await customMiddleware(req)
-//   return intlMiddleware(req)
-// }
-
-// export const config = {
-//   matcher: ['/', '/(fr|en|ja|de|ru|es|fa|ar)/:path*']
-// }
