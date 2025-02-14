65bfc2c37c91e86ec119f5a9d651f5a45e325950
Aki Inoyatov <info@gravityelements.com>
2025-02-14 12:02:22 -0700

    title columns updated

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -41,26 +41,26 @@ export default function DashboardPage() {
       <section className='bg-background-secondary py-20 max-lg:py-10'>
         <div className='mx-auto grid max-w-screen-lg grid-cols-3 gap-7 px-8 py-5 max-lg:max-w-fit max-lg:grid-cols-1 max-lg:gap-10'>
           <div className='text-center'>
-            <h2 className='mb-3  text-xl font-semibold'>{t('Approachable')}</h2>
+            <h2 className='mb-3  text-xl font-semibold'>{t('Column_1_title')}</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
               {t(
-                'Add_components_without_sending_additional_client_side_JavaScript_Built_on_the_latest_React_features'
+                'Columns_1_description'
               )}
             </p>
           </div>
           <div className='text-center'>
-            <h2 className='mb-3 text-xl font-semibold'>{t('Versatile')}</h2>
+            <h2 className='mb-3 text-xl font-semibold'>{t('Column_2_title')}</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
               {t(
-                'Automatic_Image_Font_and_Script_Optimizations_for_improved_UX_and_Core_Web_Vitals'
+                'Columns_2_description'
               )}
             </p>
           </div>
           <div className='text-center'>
-            <h2 className='mb-3 text-xl font-semibold'>{t('Performant')}</h2>
+            <h2 className='mb-3 text-xl font-semibold'>{t('Column_3_title')}</h2>
             <p className='text-text-secondary max-lg:max-w-[500px]'>
               {t(
-                'A_rich_incredibly_adoptable_template_that_scales_between_a_small_showcase_website_and_a_full_size_app'
+                'Columns_3_description'
               )}
             </p>
           </div>
