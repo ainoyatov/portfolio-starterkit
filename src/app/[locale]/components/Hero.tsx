b16863aaf06a3aa59565e414ee34ad9d3388dbf3
Aki Inoyatov <info@gravityelements.com>
2025-02-17 20:56:33 -0700

    code clean

--- src/app/[locale]/components/Hero.tsx
+++ src/app/[locale]/components/Hero.tsx
@@ -18,7 +18,6 @@ export const HeroSection: FC<Props> = ({locale}) => {
         <div>
             <section className='flex flex-col items-center justify-center py-24'>
                 <h1 className='text-center text-7xl font-extrabold leading-tight'>
-                    {/* {t('An')}{' '} */}
                     <span className='bg-span-bg bg-clip-text text-transparent'>
                         {t('Tech_Leader')}
                     </span>
@@ -26,9 +25,7 @@ export const HeroSection: FC<Props> = ({locale}) => {
                     {t('Title')}
                 </h1>
                 <div className='my-6 px-20 text-center text-2xl text-text-secondary'>
-                    {t(
-                        'Title_Description'
-                    )}
+                    {t('Title_Description')}
                 </div>
                 <div className='mt-4 flex flex-row gap-4'>
 
@@ -53,25 +50,19 @@ export const HeroSection: FC<Props> = ({locale}) => {
                     <div className='text-center'>
                         <h2 className='mb-3  text-xl font-semibold'>{t('Column_1_title')}</h2>
                         <p className='text-text-secondary max-lg:max-w-[500px]'>
-                        {t(
-                            'Columns_1_description'
-                        )}
+                            {t('Columns_1_description')}
                         </p>
                     </div>
                     <div className='text-center'>
                         <h2 className='mb-3 text-xl font-semibold'>{t('Column_2_title')}</h2>
                         <p className='text-text-secondary max-lg:max-w-[500px]'>
-                        {t(
-                            'Columns_2_description'
-                        )}
+                            {t('Columns_2_description')}
                         </p>
                     </div>
                     <div className='text-center'>
                         <h2 className='mb-3 text-xl font-semibold'>{t('Column_3_title')}</h2>
                         <p className='text-text-secondary max-lg:max-w-[500px]'>
-                        {t(
-                            'Columns_3_description'
-                        )}
+                            {t('Columns_3_description')}
                         </p>
                     </div>
                 </div>
--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,4 @@
 import { HeroSection } from './components/Hero'
-import { FC } from 'react'
 
 export default function DashboardPage({params}: {params: {locale: string}}) {
   const {locale} = params;
