a77bead179379cf65392792e6d53b193e2e6d986
Aki Inoyatov <info@gravityelements.com>
2025-05-24 17:05:16 -0700

    project descriptions updated

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -9,19 +9,19 @@ export const projects: Project[] = [
 
     {
         title: 'Investment Commpany',
-        description: 'Responsive website for a local cafe with online ordering.',
+        description: 'Tailored web solution for a trusted Arizona realty company specializing in buying and selling homes, featuring property listings, market insights, and tools for both buyers and sellers — all tailored to the unique needs of Arizona communities.',
         image: '/projects/project_1.png',
         url: 'https://uncharted-ventures.com'
     },
     {
         title: 'Picture Framing Shop',
-        description: 'Multi-language blog site built with Next.js and Contentful.',
+        description: 'Mobile-friendly eCommerce site for a local picture framing shop in Scottsdale, AZ — featuring custom framing, canvas stretching, shadowboxes, conservation framing, and art installation, with easy online browsing and local service excellence.',
         image: '/projects/project_0.png',
         url: 'https://artandcustomframes.com'
     },
     {
         title: 'Realty Company',
-        description: 'Full-featured shop with Stripe integration and product search.',
+        description: 'Responsive website for a Texas-based company specializing in buying and selling homes, featuring property listings, seller inquiry tools, and dynamic content management through Contentful.',
         image: '/projects/project_2.png',
         url: 'https://otinvestmentgroup.com'
     },
