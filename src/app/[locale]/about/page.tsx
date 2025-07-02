ee7113a56f89d710808cefaa2f3f854b5f83541f
Aki Inoyatov <info@gravityelements.com>
2025-07-01 22:04:26 -0700

    clean up

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -1,6 +1,5 @@
 import { useTranslations } from 'next-intl'
 import Image from 'next/image'
-import Link from 'next/link'
 
 export default function About({params}:{params: {locale:string}}) {
   const {locale} = params
